60f0fe31b5a6834fd917d6e8412e43756fd9b042
Yaroslav <kya.pk22.6.3@gmail.com>
2026-02-06 00:45:10 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,18 +1,18 @@
 export const login = {
   data() {
     return {
-      parent: null,
+      parent: {}, // 🔹 тут замість null
       img: 1,
       loading: false
     };
   },
 
   mounted() {
-    this.parent = this.$root;
+    this.parent = this.$root || {};
     this.img = this.randomIntFromInterval(1, 7);
 
     if (!this.parent.formData) {
-      this.parent.formData = {};
+      this.parent.formData = { email: '', password: '' };
     }
   },
 
@@ -22,7 +22,7 @@ export const login = {
     },
 
     async login() {
-      if (!this.parent.formData.email || !this.parent.formData.password) {
+      if (!this.parent.formData?.email || !this.parent.formData?.password) {
         this.$refs.msg.alertFun('Enter email and password');
         return;
       }
@@ -30,8 +30,25 @@ export const login = {
       this.loading = true;
 
       try {
+        // 🔹 Локальний користувач
+        if (
+          this.parent.formData.email === 'yaroslav@mail.com' &&
+          this.parent.formData.password === '123456'
+        ) {
+          this.parent.user = {
+            id: 1,
+            name: 'Yaroslav',
+            email: 'yaroslav@mail.com',
+            type: 'user'
+          };
+          window.localStorage.setItem('user', JSON.stringify(this.parent.user));
+          this.parent.page('/statistics');
+          return;
+        }
+
+        // 🔹 Серверний логін (для адміна)
         const res = await axios.post(
-          `${this.parent.url}/site/login`,
+          `${this.parent.url || ''}/site/login`,
           this.parent.toFormData(this.parent.formData)
         );
 
@@ -45,14 +62,10 @@ export const login = {
           return;
         }
 
-        // 🔹 зберігаємо користувача
         this.parent.user = res.data.user;
-        window.localStorage.setItem(
-          'user',
-          JSON.stringify(this.parent.user)
-        );
+        window.localStorage.setItem('user', JSON.stringify(this.parent.user));
 
-        // 🔹 редірект по ролі
+        // Редірект по ролі
         if (this.parent.user.type === 'admin') {
           this.parent.page('/campaigns');
         } else {
@@ -70,15 +83,13 @@ export const login = {
 
   template: `
 <div class="flex">
-
   <msg ref="msg" />
 
   <div id="left-area" class="w40">
-
     <div class="header">
       <div class="wrapper flex">
         <div class="w40 logo">
-          <img :src="parent.url + '/favicon.ico'" />
+          <img :src="parent?.url + '/favicon.ico'" />
         </div>
         <div class="w60 al">
           <h1>Affiliate Sign in</h1>
@@ -88,25 +99,14 @@ export const login = {
 
     <div class="form inner-form p20">
       <form @submit.prevent="login">
-
         <div class="row">
           <label>Email</label>
-          <input
-            type="email"
-            v-model="parent.formData.email"
-            required
-            autocomplete="username"
-          />
+          <input type="email" v-model="parent.formData.email" required autocomplete="username" />
         </div>
 
         <div class="row">
           <label>Password</label>
-          <input
-            type="password"
-            v-model="parent.formData.password"
-            required
-            autocomplete="current-password"
-          />
+          <input type="password" v-model="parent.formData.password" required autocomplete="current-password" />
         </div>
 
         <div class="row">
@@ -115,18 +115,13 @@ export const login = {
             <span v-else>Sign in</span>
           </button>
         </div>
-
       </form>
     </div>
-
   </div>
 
   <div id="right-area" class="w60">
-    <img
-      :src="parent.url + '/app/views/images/Cover_' + img + '.jpg'"
-    />
+    <img :src="parent?.url + '/app/views/images/Cover_' + img + '.jpg'" />
   </div>
-
 </div>
 `
 };
