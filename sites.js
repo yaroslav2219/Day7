e61c5ba3758d9c46e43145efb9d84cd6d1eb8b87
Yaroslav <kya.pk22.6.3@gmail.com>
2026-02-06 09:23:45 +0200

    Update sites.js

--- sites.js
+++ sites.js
@@ -1,6 +1,8 @@
-console.log('sites module loaded');
+console.log('payments module loaded');
 
-export const sites = {
+const API_URL = 'https://affiliate.yanbasok.com';
+
+export const payments = {
   data() {
     return {
       parent: null,
@@ -12,103 +14,71 @@ export const sites = {
   mounted() {
     this.parent = this.$root;
 
-    if (!this.parent?.user?.id || this.parent.user.role !== 'user') {
-      this.$router.push('/');
+    // ✅ правильна перевірка доступу
+    if (!this.parent?.user?.id || this.parent.user.type !== 'user') {
+      this.$router.replace('/');
       return;
     }
 
-    this.getSites();
+    this.getPayments();
   },
 
   methods: {
 
-   async getSites() {
-  this.loader = true;
-
-  try {
-    const res = await axios.post(
-      'https://affiliate.yanbasok.com/site/getUserSites',
-      this.parent.toFormData({ user_id: this.parent.user.id })
-    );
-
-    this.items = Array.isArray(res.data.items)
-      ? res.data.items
-      : [];
-  } catch (e) {
-    console.error('getSites error:', e);
-  } finally {
-    this.loader = false;
-  }
-},
-
-    async toggleSite(item, value) {
-      const old = item.active;
-      item.active = value;
+    async getPayments() {
+      this.loader = true;
 
       try {
-        await axios.post(
-          this.parent.url + '/site/actionSite',
-          this.parent.toFormData({
-            id: item.id,
-            active: value
-          })
+        const res = await axios.post(
+          API_URL + '/site/getUserPayments',
+          this.parent.toFormData({ user_id: this.parent.user.id })
         );
+
+        this.items = Array.isArray(res.data.items)
+          ? res.data.items
+          : [];
       } catch (e) {
-        console.error('toggleSite error:', e);
-        item.active = old;
+        console.error('getPayments error:', e);
+      } finally {
+        this.loader = false;
       }
     }
+
   },
 
   template: `
 <div class="inside-content">
   <Header />
 
-  <h1>Sites</h1>
+  <h1>Payments</h1>
 
   <div v-if="loader" id="spinner"></div>
 
-  <div v-if="items.length" class="table">
+  <div class="table" v-if="items.length">
     <table>
       <thead>
         <tr>
-          <th>Fraud</th>
-          <th>Leads</th>
-          <th>Clicks</th>
-          <th>Views</th>
-          <th>Site</th>
-          <th></th>
+          <th class="id">#</th>
+          <th>Description</th>
+          <th>Date</th>
+          <th>Value</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr v-for="item in items" :key="item.id">
-          <td>{{ item.fclicks || 0 }}</td>
-          <td>{{ item.leads || 0 }}</td>
-          <td>{{ item.clicks || 0 }}</td>
-          <td>{{ item.views || 0 }}</td>
-
-          <td>
-            <a :href="item.site" target="_blank">
-              {{ item.site }}
-            </a>
-          </td>
-
-          <td class="actions">
-            <toogle
-              :modelValue="item.active"
-              @update:modelValue="toggleSite(item, $event)"
-            />
-          </td>
+        <tr v-for="(item, index) in items" :key="item.id || index">
+          <td class="id">{{ index + 1 }}</td>
+          <td>{{ item.description }}</td>
+          <td>{{ item.date_title || item.date }}</td>
+          <td>{{ item.value }}</td>
         </tr>
       </tbody>
     </table>
   </div>
 
-  <div v-else class="empty">
-    No sites
+  <div class="empty" v-else>
+    No payments
   </div>
 </div>
 `
 };
-
